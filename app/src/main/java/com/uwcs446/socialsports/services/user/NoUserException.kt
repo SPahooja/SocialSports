88e498b6ec3589d3fb0a04fef146156ecd964ed6
SPahooja <55768732+SPahooja@users.noreply.github.com>
2021-06-12 17:50:45 +0530

    Feature/add user service (#8)
    * add firebase integration
    
    * add google services config
    
    * add user service
    
    * add user service

---
+++ app/src/main/java/com/uwcs446/socialsports/services/user/NoUserException.kt
@@ -0,0 +1,3 @@
+package com.uwcs446.socialsports.services.user
+
+class NoUserException : Throwable()
---
+++ app/src/main/java/com/uwcs446/socialsports/services/user/User.kt
@@ -0,0 +1,16 @@
+package com.uwcs446.socialsports.services.user
+
+import com.google.firebase.auth.FirebaseUser
+
+data class User(
+    val id: String
+) {
+    companion object {
+        fun from(firebaseUser: FirebaseUser?): User? {
+            firebaseUser ?: return null
+            return User(
+                id = firebaseUser.uid
+            )
+        }
+    }
+}
