4662487e2f75fa2714153ceeb4ca00c3302c4a20
Simon Liu <cyamonide@gmail.com>
2021-07-02 00:49:13 -0400

    Add additional fields

--- app/src/main/java/com/uwcs446/socialsports/services/match/Match.kt
+++ app/src/main/java/com/uwcs446/socialsports/services/match/Match.kt
@@ -1,12 +1,16 @@
 package com.uwcs446.socialsports.services.match
 
+import com.google.type.DateTime
 import com.uwcs446.socialsports.services.user.User
 
 data class Match(
     val id: String,
     val type: MatchType,
+    val title: String,
+    val description: String,
+    val time: DateTime,
     val host: User,
-    val participants: Set<User>
+    val participants: Set<User>,
 ) {
     // TODO (simon): connect to firestore
 }
