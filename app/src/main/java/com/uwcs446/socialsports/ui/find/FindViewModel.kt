8459195a8120273bfe3d0e6110e6e389539d74b0
Simon Liu <cyamonide@gmail.com>
2021-07-08 02:31:20 -0400

    Implement match filtering by sport (#24)
    ## Description
    
    This PR implements match filtering by sport.
    
    ## Minor changes
    
    - Refactored a bit of `FindFragment` for style, and so that the Recycler view updated properly
    - Changed the argument of `filterMatchBySport` to use the `Sport` enum instead of a `String`

--- app/src/main/java/com/uwcs446/socialsports/ui/find/FindViewModel.kt
+++ app/src/main/java/com/uwcs446/socialsports/ui/find/FindViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.uwcs446.socialsports.domain.match.Match
+import com.uwcs446.socialsports.domain.match.Sport
 import com.uwcs446.socialsports.ui.matchlist.MatchListUtils
 import java.time.LocalDate
 
@@ -22,6 +23,12 @@ class FindViewModel : ViewModel() {
     // TODO: filter match by time, remove filtering if both values are -1
     fun filterMatchByTime(hour: Int, minute: Int) {}
 
-    // TODO: filter match by type, remove filtering if type is "ALL"
-    fun filterMatchByType(type: String) {}
+    /**
+     * Filters matches by sport. Shows all matches if sport is `null`.
+     */
+    fun filterMatchBySport(sport: Sport?) {
+        _matchList.value = MatchListUtils.genFakeMatchData(5).filter {
+            if (it.sport == null) true else it.sport == sport
+        }
+    }
 }
