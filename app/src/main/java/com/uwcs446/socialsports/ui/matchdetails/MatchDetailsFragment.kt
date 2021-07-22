f1cee5900991bbdd8b40a94df771740d84446dcf
Shiyi Li <73145814+anthea-li@users.noreply.github.com>
2021-07-21 21:31:25 -0400

    Enhancements to match details page ui (#39)
    * add edit button, teams to details page; fix sport type filtering bug; update sport type icon

--- app/src/main/java/com/uwcs446/socialsports/ui/matchdetails/MatchDetailsFragment.kt
+++ app/src/main/java/com/uwcs446/socialsports/ui/matchdetails/MatchDetailsFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.uwcs446.socialsports.databinding.FragmentMatchDetailsBinding
 import com.uwcs446.socialsports.ui.matchlist.MatchListUtils
 import java.time.format.DateTimeFormatter
@@ -17,6 +19,7 @@ class MatchDetailsFragment : Fragment() {
     private val args: MatchDetailsFragmentArgs by navArgs()
     private var _binding: FragmentMatchDetailsBinding? = null
     private val binding get() = _binding!!
+    private val isHost: Boolean = true // TODO: set boolean
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,9 +31,11 @@ class MatchDetailsFragment : Fragment() {
         val root: View = binding.root
 
         // Placeholder find the match that matches the passed in ID
-        val matches = MatchListUtils.genFakeMatchData(args.matchId!!.toInt())
+//        val matches = MatchListUtils.genFakeMatchData(args.matchId!!.toInt())
+//
+//        val match = matches.find { match -> match.id == args.matchId }
 
-        val match = matches.find { match -> match.id == args.matchId }
+        val match = MatchListUtils.genFakeMatchData(1)[0] // TODO: this mock for easy testing, replace it
 
         // Views
         val titleTextView: TextView = binding.matchTitle
@@ -55,6 +60,29 @@ class MatchDetailsFragment : Fragment() {
             addressTextView.setText("1873 Bloor St W, Toronto, ON M6R 2Z") // TODO: add location address field
             descriptionTextView.setText(match.description)
             hostNameTextView.setText("John Smith") // TODO: Use names from user
+        }
+
+        // set up recycler view for two teams
+        val teamSize = match.sport.teamSize
+        // TODO: replace mocks - get player name or some other identifier from match.teamOne and match.teamTwo
+        val teamOne = listOf("John Smith", "John Smith", "John Smith")
+        val teamTwo = listOf("John Smith", "John Smith", "John Smith", "John Smith", "John Smith", "John Smith", "John Smith", "John Smith", "John Smith", "John Smith")
+
+        val teamOneViewAdapter = TeamListAdapter(teamOne, teamSize)
+        binding.layoutTeamOne.recyclerviewTeam.setHasFixedSize(true)
+        binding.layoutTeamOne.recyclerviewTeam.stopNestedScroll()
+        binding.layoutTeamOne.recyclerviewTeam.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        binding.layoutTeamOne.recyclerviewTeam.adapter = teamOneViewAdapter
+
+        val teamTwoViewAdapter = TeamListAdapter(teamTwo, teamSize)
+        binding.layoutTeamTwo.recyclerviewTeam.setHasFixedSize(true)
+        binding.layoutTeamTwo.recyclerviewTeam.stopNestedScroll()
+        binding.layoutTeamTwo.recyclerviewTeam.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        binding.layoutTeamTwo.recyclerviewTeam.adapter = teamTwoViewAdapter
+
+        // display the edit button if current user is the match host
+        if (isHost) {
+            binding.editButtonMatchDetails.visibility = View.VISIBLE
         }
 
         return root
