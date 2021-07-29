89c0450ea52607b71d829f3f3eb465a9966aade3
Simon Liu <cyamonide@gmail.com>
2021-07-28 23:54:46 -0400

    fix ci

--- app/src/main/java/com/uwcs446/socialsports/services/user/FirebaseUserRepository.kt
+++ app/src/main/java/com/uwcs446/socialsports/services/user/FirebaseUserRepository.kt
@@ -1,6 +1,5 @@
 package com.uwcs446.socialsports.services.user
 
-import android.util.Log
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentSnapshot
 import com.uwcs446.socialsports.di.module.UsersCollection
@@ -12,8 +11,6 @@ class FirebaseUserRepository(
     @UsersCollection
     private val usersCollection: CollectionReference
 ) : UserRepository {
-
-    private val TAG = this::class.simpleName
 
     override suspend fun findById(id: String): User? {
         return usersCollection
